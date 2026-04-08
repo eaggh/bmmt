d83e5559fa06614c85705331fcbce958b73e5838
eaggh <eaghagbon9@gmail.com>
2026-04-08 13:34:49 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5000',
   'http://localhost:5173/',
   'https://bellskeet.netlify.app',
-  'https://bellbizca-omega.vercel.app'
-  'https://bellbizca-omega.vercel.app'
+  'https://bellbizca-omega.vercel.app',
    // add all your trusted frontends here
 ];
 
